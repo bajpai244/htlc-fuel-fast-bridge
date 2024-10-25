9a280e1afc66cf9a7fadfb1d681c2d15f93ead96
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-10-26 01:34:14 +0700

    feat: add test for time lock already existing ⏰

--- packages/fuel/tests/tests.test.ts
+++ packages/fuel/tests/tests.test.ts
@@ -477,6 +477,92 @@ describe("HTCL tests", async () => {
 		}
 	});
 
+    test("test time lock fail, lock already exists", async () => {
+		const launched = await launchTestNode({
+			walletsConfig: {
+				count: 5,
+				assets: 1,
+				amountPerCoin: 10000000,
+			},
+			contractsConfigs: [
+				{
+					factory: ContractsFactory,
+					walletIndex: 0,
+				},
+			],
+		});
+
+		const secret = "mysecret";
+		const hash = sha256(toUtf8Bytes(secret));
+
+		const amountToLock = bn(10000);
+		const fee = bn(10);
+
+		// number of blocks for 1 hour of time to pass
+		const ONE_HOUR = 1 * 60 * 60;
+
+		const {
+			wallets: [sender, destination],
+			contracts: [contract],
+		} = launched;
+
+		const provider = sender.provider;
+
+		const baseAssetId = provider.getBaseAssetId();
+		const txGasLimit = provider.getGasConfig().maxGasPerTx.sub(100000);
+
+		// expressed in block time, block time 1 second
+		// seet to 1 hour here
+		const expiryTimeSeconds = (await provider.getBlockNumber()).add(ONE_HOUR);
+
+		const lock: LockInput = {
+			token: { bits: baseAssetId },
+			sender: { bits: sender.address.toB256() },
+			destination: { bits: destination.address.toB256() },
+			hash,
+			balance: amountToLock,
+			expiryTimeSeconds,
+			fee,
+		};
+
+		const { value: lockHash } = await contract.functions
+			.compute_lock_hash(lock)
+			.get();
+		console.log("lockHash: ", lockHash);
+
+		const previousBalanceOfContract = await contract.getBalance(baseAssetId);
+		console.log("previous balance of contract", previousBalanceOfContract);
+
+		const { waitForResult } = await contract.functions
+			.time_lock(lock)
+			.callParams({
+				forward: [amountToLock, baseAssetId],
+			})
+			.txParams({
+				gasLimit: txGasLimit,
+			})
+			.call();
+
+		const { value, transactionResult } = await waitForResult();
+
+        try {
+			await contract.functions
+				.time_lock(lock)
+				.callParams({
+					forward: [amountToLock, baseAssetId],
+				})
+				.txParams({
+					gasLimit: txGasLimit,
+				})
+				.call();
+		} catch (error) {
+			expect(error.message).toBe(
+				`The transaction reverted because a "require" statement has thrown "LockAlreadyExists".`,
+			);
+			expect(error.code).toBe(ErrorCode.SCRIPT_REVERTED);
+		}
+	});
+
 	test("test time lock pass", async () => {
 		const launched = await launchTestNode({
 			walletsConfig: {
